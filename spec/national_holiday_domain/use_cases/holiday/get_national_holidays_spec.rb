3c3cfd6d84c216341a1ef6d5dd9c89d78929fbb0
Marco Damaceno <maadamaceno@gmail.com>
2021-06-29 00:48:38 -0300

    test the type of returned values

--- spec/national_holiday_domain/use_cases/holiday/get_national_holidays_spec.rb
+++ spec/national_holiday_domain/use_cases/holiday/get_national_holidays_spec.rb
@@ -4,20 +4,18 @@ require 'spec_helper'
 
 describe NationalHolidayDomain::UseCases::Holiday::GetNationalHolidays do
   let(:klass) { described_class.new }
-  let(:holiday_model) { NationalHolidayDomain::Entities::Holiday }
 
   describe '#call' do
-    before do
-      allow(klass)
-        .to receive(:get_in_database)
-        .with(an_instance_of(String), an_instance_of(Hash))
-        .and_return([holiday_model.new])
-    end
-
     it 'returns a hash with an array of holidays' do
       result = klass.call('BR')
 
-      expect(result[:holidays][0]).to be_instance_of holiday_model
+      expect(result[:holidays][0]).to include(
+        name: a_kind_of(String),
+        month_day: a_kind_of(String),
+        owner: a_kind_of(String),
+        type: a_kind_of(String),
+        optional: be(true).or(be(false))
+      )
     end
   end
 end
