bc719eab5795d6140de5e5943a187b566699d809
Marco Damaceno <maadamaceno@gmail.com>
2021-06-28 23:38:37 -0300

    add method to return an instance of database

---
+++ lib/national_holiday_domain/database_context/concerns/database.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+#
+module NationalHolidayDomain
+  module DatabaseContext
+    module Concerns
+      module Database
+        def self.db_instance(country_abbreviation)
+          NationalHolidayDomain::DatabaseContext::NationalHolidayContext.new(country_abbreviation.downcase)
+        end
+      end
+    end
+  end
+end
