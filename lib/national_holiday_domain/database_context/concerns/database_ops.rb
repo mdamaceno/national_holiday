ad7d71a299221f21da8a7719a83e40f0db68dfc8
Marco Damaceno <maadamaceno@gmail.com>
2021-06-29 13:53:08 -0300

    include id in filter

--- lib/national_holiday_domain/database_context/concerns/database_ops.rb
+++ lib/national_holiday_domain/database_context/concerns/database_ops.rb
@@ -6,6 +6,7 @@ module NationalHolidayDomain
       module DatabaseOps
         def self.find_in_database(collection, filter_by = {})
           collection = collection.select { |r| r[:type] === filter_by[:type] } unless filter_by[:type].nil?
+          collection = collection.select { |r| r[:id] === filter_by[:id] } unless filter_by[:id].nil?
 
           collection
         end
