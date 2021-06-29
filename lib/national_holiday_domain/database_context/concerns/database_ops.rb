32f1ffa6f2f1f9b1ce130fd42852b8b08a973643
Marco Damaceno <maadamaceno@gmail.com>
2021-06-29 00:04:29 -0300

    remove unnecessary map

--- lib/national_holiday_domain/database_context/concerns/database_ops.rb
+++ lib/national_holiday_domain/database_context/concerns/database_ops.rb
@@ -5,14 +5,11 @@ module NationalHolidayDomain
     module Concerns
       module DatabaseOps
         def self.find_in_database(collection, filter_by = {})
-          result = collection
-            .map { |c| c.to_h }
-
           unless filter_by[:type].nil?
-            result = result.select { |r| r[:type] === filter_by[:type] }
+            collection = collection.select { |r| r[:type] === filter_by[:type] }
           end
 
-          result
+          collection
         end
       end
     end
